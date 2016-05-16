fda4c15eb1a20ef650bda014f0a8b918fe61caf6
Claudia Green <claudia@claudiag.me.uk>
2016-05-16 12:02:28 +0100

    3rd commit

--- lib/leap_year.rb
+++ lib/leap_year.rb
@@ -1,7 +1,13 @@
 class Year
 
-  def is_a_leap_year(input)
+  def is_a_leap_year?(input)
     raise "Input is not an integer" if !(input.is_a? Integer)
+    if input % 4 == 0 || input % 400 == 0
+      true
+    else
+      false
+    end
+
   end
 
 end
--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -6,9 +6,15 @@ describe Year do
 
   it "raises an error if input is not an integer" do
     message = "Input is not an integer"
-    expect{ year.is_a_leap_year("string") }.to raise_error message
+    expect{ year.is_a_leap_year?("string") }.to raise_error message
   end
 
+  it "returns true for 1996" do
+    expect(year.is_a_leap_year?(1996)).to be(true)
+  end
 
+  it "returns false for 2001" do
+    expect(year.is_a_leap_year?(2001)).to be(false)
+  end
 
 end
